6805c9d9959768266fd9bdc6e20d10b928ef0d81
Colin Stewart <44102263+J-Colin-Stewart@users.noreply.github.com>
2022-10-04 21:29:24 -0500

    Final project updates

---
+++ Assets/Scripts/SeeThrough.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeThrough : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponent<Renderer>())
+        {
+            GetComponent<Renderer>().material.renderQueue = 3002;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # SEP21-XRD-ARXray
+
 Third project in XR Development course (CircuitStream)
-
-# This is a header
-
-## This is a sub-header
-
-This is a normal text **BOLD** _Italic_
